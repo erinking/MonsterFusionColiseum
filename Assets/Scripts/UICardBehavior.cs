459e71beb42a15fcb1835d98e6f788ee838d8bea
Ari Green <arigreen@mit.edu>
2016-03-10 19:23:04 -0500

    Game Breaking Bug Fixed, Cards Selectable

--- Assets/Scripts/UICardBehavior.cs
+++ Assets/Scripts/UICardBehavior.cs
@@ -1,22 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UICardBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		//This is a workaround because delegate onClick.AddListener assignments aren't working in the GameMainControl script.
+		//Issue is due to delegate assignments in a for loop taking the last value assigned.
+		gameObject.GetComponent<Button> ().onClick.AddListener (() => GameObject.Find ("EventSystem").GetComponent<GameMainControl>().PickCard(gameObject.name) );
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	void OnMouseDown()
-	{
-		print ("hi");
-		//This is a workaround because delegate onClick.AddListener assignments aren't working.
-		GameObject.Find ("EventSystem").GetComponent<GameMainControl>().PickCard(gameObject.name);
 	}
 }
